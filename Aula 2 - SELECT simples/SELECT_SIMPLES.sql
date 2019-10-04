85e972bb930afdd2e823a03804b28da3418ac29b
Gilis Santos <53980236+gilissantos@users.noreply.github.com>
2019-10-04 20:25:40 -0300

    Add files via upload

--- Aula 2 - SELECT simples/SELECT_SIMPLES.sql
+++ Aula 2 - SELECT simples/SELECT_SIMPLES.sql
@@ -1,6 +1,34 @@
-SELECT		SUM(p.UnitPrice) AS Soma,
-			AVG(p.UnitPrice) AS Media,
-			MIN(p.UnitPrice) AS Minimo,
-			MAX(p.UnitPrice) AS Maximo
-FROM		Products as p;
+/*
 
+O SELECT simples utilizamos o caractere especial * para recuperar todas as colunas de uma tabela.
+Em boas práticas não devemos utilizá-lo, o ideal é colocar os nomes das colunas que queremos recuperar os dados.
+
+
+Sintáxe:
+
+SELECT -> SELECIONE
+  *    -> [Todas as colunas]	
+FROM   -> [nome da tabela]
+		
+*/
+
+
+
+
+SELECT * FROM Employees;
+
+
+
+EmployeeID		LastName		FirstName		Title
+1				Davolio			Nancy			Sales Representative
+2				Fuller			Andrew			Vice President, Sales
+3				Leverling		Janet			Sales Representative
+4				Peacock			Margaret		Sales Representative
+5				Buchanan		Steven			Sales Manager
+6				Suyama			Michael			Sales Representative
+7				King			Robert			Sales Representative
+8				Callahan		Laura			Inside Sales Coordinator
+9				Dodsworth		Anne			Sales Representative
+
+
+ -- Observação: Neste exemplo resulta em 9 linhas e 4 colunas(porém nesta tabela há 14 colunas) 
