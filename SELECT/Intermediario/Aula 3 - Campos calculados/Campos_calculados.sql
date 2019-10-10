074a9697e54776c83648edd61155c5d03e9aa3a0
Gilis Santos <53980236+gilissantos@users.noreply.github.com>
2019-10-10 19:01:01 -0300

    Update Campos_calculados.sql

--- SELECT/Intermediario/Aula 3 - Campos calculados/Campos_calculados.sql
+++ SELECT/Intermediario/Aula 3 - Campos calculados/Campos_calculados.sql
@@ -1,8 +1,8 @@
 /*
-	Neste exemplo vamos usar a tabela Products e criar um campo tempor·rio.
-	O campo/coluna tempor·ria vai armazenar o resultado do cl·culo dos campos UnitPrice x UnitsInStock.
+	Neste exemplo vamos usar a tabela Products e criar um campo tempor√°rio.
+	O campo/coluna tempor√°ria vai armazenar o resultado do cl√°culo dos campos UnitPrice x UnitsInStock.
 	
-	Sint·xe:
+	Sint√°xe:
 
 	SELECT nameColumn1, (nameColumn2 * nameColumn3) AS 'nameSolicited' FROM nameTable;
 	 
@@ -13,20 +13,20 @@
 -- Query of example:
 
 SELECT		ProductName,
-			UnitPrice,
-			UnitsInStock,
-			(UnitPrice * UnitsInStock) AS ValueTotal
+		UnitPrice,
+		UnitsInStock,
+		(UnitPrice * UnitsInStock) AS ValueTotal
 FROM		Products; 
 
 
 -- Resultado da Query:
 
-ProductName						UnitPrice	UnitsInStock	ValueTotal
-Chai							18,00		39				702,00
-Chang							19,00		17				323,00
-Aniseed Syrup					10,00		13				130,00
-Chef Anton's Cajun Seasoning	22,00		53				1166,00
-Chef Anton's Gumbo Mix			21,35		0				0,00
+ProductName				UnitPrice	UnitsInStock	ValueTotal
+Chai					18,00		39		702,00
+Chang					19,00		17		323,00
+Aniseed Syrup				10,00		13		130,00
+Chef Anton's Cajun Seasoning		22,00		53		1166,00
+Chef Anton's Gumbo Mix			21,35		0		0,00
 
 
--- Obs: para efetuar calculos devemos verificar se o tipo de dados do campo È n˙merico ou data/hora.
+-- Obs: Para efetuar calculos devemos verificar se o tipo de dados do campo √© n√∫merico ou data/hora.
